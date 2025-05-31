591c737679906eb38599265a5620ba1bd221c24c
trandangtuan <58521377+trandangtuan@users.noreply.github.com>
2025-06-01 01:47:15 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'debt-manager-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Cài đặt cache ban đầu
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Caching app shell');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Intercept request
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
